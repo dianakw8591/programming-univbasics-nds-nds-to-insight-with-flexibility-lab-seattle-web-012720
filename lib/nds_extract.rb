98ff35f513f846a1c49444b13b2bda49d0b5414d
Diana Wendt <dianakw@gmail.com>
2020-01-10 23:05:34 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -74,7 +74,11 @@ def gross_per_studio(collection)
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
   
+  result = {}
+  index = 0
   
+  while i < collection.length do
+    studio_name = collection[index][:studio_name]
 end
 
 def movies_with_directors_set(source)
